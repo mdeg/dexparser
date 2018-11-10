f9112f785521edb10e1a0d9b01d4bc2fee54f15c
Michael Degano <michael@dega.no>
2018-11-10 16:32:07 +0700

    Added parsing for string, type, proto, field and method IDs

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,11 +1,13 @@
 use nom::*;
 
+// The magic that starts a DEX file
 const DEX_FILE_MAGIC: [u8; 4] = [0x64, 0x65, 0x78, 0x0a];
-
 // Indicates standard (little-endian) encoding
 const ENDIAN_CONSTANT: [u8; 4] = [0x12, 0x34, 0x56, 0x78];
 // Indicates modified non-standard (big-endian) encoding
 const REVERSE_ENDIAN_CONSTANT: [u8; 4] = [0x78, 0x56, 0x34, 0x12];
+
+const NO_INDEX: u32 = 0xffffffff;
 
 #[derive(Debug)]
 pub struct DexFile<'a> {
@@ -79,22 +81,93 @@ pub fn parse(buffer: &[u8]) -> Result<DexFile, ParserErr> {
 
 named_args!(parse_dex_file ( e: nom::Endianness ) <&[u8], DexFile>,
     do_parse!(
-        header: apply!(parse_header, e)                     >>
-        string_ids: apply!(string_identifier_list, e, header.string_ids_size)       >>
-
+        header: apply!(parse_header, e)                                                         >>
+        string_id_idxs: count!(u32!(e), header.string_ids_size as usize)                     >>
+        type_id_idxs: count!(u32!(e), header.type_ids_size as usize)                    >>
+        proto_id_idxs: apply!(proto_id_items, e, header.proto_ids_size as usize)               >>
+        field_id_idxs: apply!(field_id_items, e, header.field_ids_size as usize)               >>
+        method_id_idxs: apply!(method_id_items, e, header.method_ids_size as usize)            >>
+        class_def_idxs: apply!(class_def_items, e, header.class_defs_size as usize)     >>
         (DexFile { header })
 ));
 
-struct StringIdentifierOffsetList {
-    // String data offsets
-    string_data_offsets: Vec<u32>
+named_args!(proto_id_items ( e: nom::Endianness, s: usize ) <&[u8], Vec<ProtoIdItem> >,
+    count!(
+        do_parse!(
+            shorty_idx: u32!(e)         >>
+            return_type_idx: u32!(e)    >>
+            parameters_off: u32!(e)     >>
+            (ProtoIdItem { shorty_idx, return_type_idx, parameters_off })
+        ), s)
+);
+
+struct ProtoIdItem {
+    // Index into the string IDs list for the descriptor string of this prototype
+    shorty_idx: u32,
+    // Index into the type_ids list for the return type of this prototype
+    return_type_idx: u32,
+    parameters_off: u32
 }
 
-named_args!(string_identifier_list ( e: nom::Endianness, num: u32 ) <&[u8], StringIdentifierOffsetList>,
-    do_parse!(
-        string_data_offsets: count!(u32!(e), num as usize) >>
-    (StringIdentifierOffsetList { string_data_offsets })
-));
+named_args!(field_id_items ( e: nom::Endianness, s: usize ) <&[u8], Vec<FieldIdItem> >,
+    count!(
+        do_parse!(
+            class_idx: u16!(e)  >>
+            type_idx: u16!(e)   >>
+            name_idx: u32!(e)   >>
+            (FieldIdItem { class_idx, type_idx, name_idx })
+        ), s)
+);
+
+struct FieldIdItem {
+    class_idx: u16,
+    type_idx: u16,
+    name_idx: u32
+}
+
+named_args!(method_id_items ( e: nom::Endianness, s: usize ) <&[u8], Vec<MethodIdItem> >,
+    count!(
+        do_parse!(
+            class_idx: u16!(e)  >>
+            proto_idx: u16!(e)   >>
+            name_idx: u32!(e)   >>
+            (MethodIdItem { class_idx, proto_idx, name_idx })
+        ), s)
+);
+
+struct MethodIdItem {
+    class_idx: u16,
+    proto_idx: u16,
+    name_idx: u32
+}
+
+named_args!(class_def_items ( e: nom::Endianness, s: usize ) <&[u8], Vec<ClassDefItem> >,
+    count!(
+        do_parse!(
+            class_idx: u32!(e)                  >>
+            access_flags: u32!(e)               >>
+            superclass_idx: u32!(e)             >>
+            interfaces_off: u32!(e)             >>
+            source_file_idx: u32!(e)            >>
+            annotations_off: u32!(e)            >>
+            class_data_off: u32!(e)             >>
+            static_values_off: u32!(e)          >>
+
+            (ClassDefItem { class_idx, access_flags, superclass_idx, interfaces_off,
+            source_file_idx, annotations_off, class_data_off, static_values_off})
+        ), s)
+);
+
+struct ClassDefItem {
+    class_idx: u32,
+    access_flags: u32,
+    superclass_idx: u32,
+    interfaces_off: u32,
+    source_file_idx: u32,
+    annotations_off: u32,
+    class_data_off: u32,
+    static_values_off: u32
+}
 
 named_args!(parse_header ( e: nom::Endianness ) <&[u8], Header>,
     do_parse!(
